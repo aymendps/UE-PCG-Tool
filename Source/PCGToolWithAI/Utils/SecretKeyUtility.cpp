9827b2449a4f59b1c4c9582f84be05bfc143f247
aymendps <hammami.aym@outlook.com>
2024-03-22 10:52:14 +0100

    added static function to get secret key value by name

---
+++ Source/PCGToolWithAI/Utils/SecretKeyUtility.cpp
@@ -0,0 +1,32 @@
+﻿// This capstone project was developed by Aymen Hammami - 2024
+
+
+#include "SecretKeyUtility.h"
+
+FString USecretKeyUtility::GetSecretKey(const FString& KeyName)
+{
+	TArray<FString> Lines;
+	
+	// If the file is found
+	if(FFileHelper::LoadFileToStringArray(Lines, *SecretKeyFilePath))
+	{
+		for(const FString& Line : Lines)
+		{
+			// If key name is found
+			if(Line.StartsWith(KeyName.TrimStartAndEnd()))
+			{
+				// If key value is found
+				if(int32 Index; Line.FindChar('=', Index))
+				{
+					return Line.RightChop(Index + 1).TrimStartAndEnd();
+				}
+			}
+		}
+	} else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to load secret keys file with path: %s"), *SecretKeyFilePath);
+	}
+
+	// Return an empty string if the key is not found or the file is not found
+	return "";
+}
---
+++ Source/PCGToolWithAI/Utils/SecretKeyUtility.h
@@ -0,0 +1,22 @@
+﻿// This capstone project was developed by Aymen Hammami - 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SecretKeyUtility.generated.h"
+
+/**
+ * Utility class for retrieving secret keys.
+ */
+UCLASS()
+class PCGTOOLWITHAI_API USecretKeyUtility : public UObject
+{
+	GENERATED_BODY()
+private:
+	inline static FString SecretKeyFilePath = FPaths::ProjectConfigDir() / TEXT("SecretKeys.ini");
+public:
+	// Finds and returns the secret key for the given key name. Returns an empty string if the key is not found.
+	UFUNCTION(BlueprintPure, Category = "Secret Key Utility")
+	static FString GetSecretKey(const FString& KeyName);
+};
