123f18553106024096e65403478f638ba27d4664
aymendps <hammami.aym@outlook.com>
2024-03-27 13:34:12 +0100

    added utility class for EPcgTheme to convert it to string

---
+++ Source/PCGToolWithAI/Utils/PCGThemeUtility.cpp
@@ -0,0 +1,30 @@
+﻿// This capstone project was developed by Aymen Hammami - 2024
+
+#include "PCGThemeUtility.h"
+
+FString UPCGThemeUtility::ThemeEnumToString(const EPcgTheme Theme)
+{
+	switch (Theme)
+	{
+		case EPcgTheme::Stylized:
+			return TEXT("Stylized");
+		case EPcgTheme::Northern:
+			return TEXT("Northern");
+		case EPcgTheme::None:
+			return TEXT("None");
+		default:
+			return TEXT("Invalid");
+	}
+}
+
+FString UPCGThemeUtility::GetThemeNamesWithIndices()
+{
+	FString ThemeNamesWithIndices;
+	
+	for (int32 Index = 0; Index < static_cast<int32>(EPcgTheme::None); ++Index)
+	{
+		ThemeNamesWithIndices += FString::Printf(TEXT("%d: %s | "), Index, *ThemeEnumToString(static_cast<EPcgTheme>(Index)));
+	}
+
+	return ThemeNamesWithIndices;
+}
---
+++ Source/PCGToolWithAI/Utils/PCGThemeUtility.h
@@ -0,0 +1,26 @@
+﻿// This capstone project was developed by Aymen Hammami - 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PCGToolWithAI/Enums/PCGTheme.h"
+#include "UObject/Object.h"
+#include "PCGThemeUtility.generated.h"
+
+/**
+ * Utility class for converting the PCG theme enum to string
+ */
+UCLASS()
+class PCGTOOLWITHAI_API UPCGThemeUtility : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	// Convert the PCG theme enum to a string
+	UFUNCTION(BlueprintPure, Category = "PCG Theme Utility")
+	static FString ThemeEnumToString(const EPcgTheme Theme);
+
+	// Return all PCG theme enum elements with their corresponding indices in a single string
+	UFUNCTION(BlueprintPure, Category = "PCG Theme Utility")
+	static FString GetThemeNamesWithIndices();
+};
